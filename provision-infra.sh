07db35b80d559467ed90dfe73305da1343855f54
Emmanuel Mortoo <emortoo@yahoo.com>
2024-04-22 19:02:45 -0400

    updated content

--- provision-infra.sh
+++ provision-infra.sh
@@ -1,17 +1,17 @@
-#!bin/bash
+#!/bin/bash
 
 # cd ./infrastracture/02-Terraform/pipeline-dev/userdata/
 # sh bastion-setup.sh
 
-cd ./infrastructure/02-Terraform/pipeline-dev 
+cd ~/Devops-project-2024/infrastructure/02-Terraform/pipeline-dev 
 terraform init
 terraform plan 
 terraform apply --auto-approve 2> ./error.log 
 
 
-cd ~/infrastructure/03-Ansible/playbook
+cd ~/Devops-project-2024/infrastructure/03-Ansible/playbook
 
-ansible-playbook -i host.ini ping.yml 2> ~/ping-output.txt
+ansible-playbook -i host.ini ping.yml 2> ./ping-output.txt
 ansible-playbook -i host.ini docker-sonarqube.yml
 ansible-playbook -i host.ini k8s-master-node.yml
 ansible-playbook -i host.ini k8s-worker-node.yml
